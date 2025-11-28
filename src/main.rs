0e8625865971f8757b3b40f0c46daa1b38b2d75e
toghroltp <toghroltheprogrammer@gmail.com>
2025-11-28 17:31:47 +0330

    Add functionality to serve static files from "/static" folder

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 mod handlers;
 mod templates;
 
+
 use axum::{Router, routing::get};
+use tower_http::services::ServeDir;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    const BIND_ADDRESS: &str = "0.0.0.0:3000";
-    let app = Router::new().route("/", get(handlers::home::home));
-    let listener = tokio::net::TcpListener::bind(BIND_ADDRESS).await.unwrap();
+    let bind_address = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    println!("The program is running on ip and port http://{}/", BIND_ADDRESS);
+    let app = Router::new()
+        .route("/", get(handlers::home::home))
+        .nest_service("/static", ServeDir::new("static"));
+    let listener = tokio::net::TcpListener::bind(bind_address).await.unwrap();
+
+    println!("The program is running on ip and port http://{}/", bind_address);
 
     axum::serve(listener, app).await.unwrap();
 }
