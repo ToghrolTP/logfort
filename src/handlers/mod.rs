09a8f67d4c9c2d4ef4f0d91c7b742351e81b9664
toghroltp <toghroltheprogrammer@gmail.com>
2025-11-29 07:31:29 +0330

    Add routing for contact_info handler and moving "routes" to src/handlers/mod.rs for better modularity

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,1 +1,12 @@
+use crate::handlers;
+use axum::{Router, routing::get};
+use tower_http::services::ServeDir;
+
 pub mod home;
+
+pub fn setup_routes() -> Router {
+    Router::new()
+        .route("/", get(handlers::home::home))
+        .route("/contact-info", get(handlers::home::contact_info))
+        .nest_service("/static", ServeDir::new("static"))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 mod handlers;
 mod templates;
 
-
-use axum::{Router, routing::get};
-use tower_http::services::ServeDir;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     let bind_address = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    let app = Router::new()
-        .route("/", get(handlers::home::home))
-        .nest_service("/static", ServeDir::new("static"));
+    let app = handlers::setup_routes();
     let listener = tokio::net::TcpListener::bind(bind_address).await.unwrap();
 
     println!("The program is running on ip and port http://{}/", bind_address);
