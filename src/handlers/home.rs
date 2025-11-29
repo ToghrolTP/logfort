b0cea28ba7e62d3eab7d6182afe1dc41b97ebc7b
toghroltp <toghroltheprogrammer@gmail.com>
2025-11-29 07:30:08 +0330

    Add handler for contact_info

--- src/handlers/home.rs
+++ src/handlers/home.rs
@@ -1,10 +1,21 @@
 use askama::Template;
 use axum::response::{Html, IntoResponse};
 
-use crate::templates::home::HomeTemplate;
+use crate::templates::{
+    home::HomeTemplate, 
+    contact_info::ContactInfoTemplate
+};
 
 pub async fn home() -> impl IntoResponse {
     let template = HomeTemplate {
+        site_name: "logfort".to_string(),
+    };
+
+    Html(template.render().unwrap())
+}
+
+pub async fn contact_info() -> impl IntoResponse {
+    let template = ContactInfoTemplate {
         site_name: "logfort".to_string(),
     };
 
