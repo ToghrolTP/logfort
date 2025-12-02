a4b1e018ff9204eadb73b4a909f0370842bc3f99
toghroltp <toghroltheprogrammer@gmail.com>
2025-12-02 12:54:09 +0330

    Add "About Me" page. Remove fields that are not configured globally.

--- src/handlers/home.rs
+++ src/handlers/home.rs
@@ -2,22 +2,23 @@ use askama::Template;
 use axum::response::{Html, IntoResponse};
 
 use crate::templates::{
-    home::HomeTemplate, 
-    contact_info::ContactInfoTemplate
+    home::{ HomeTemplate, ContactInfoTemplate, AboutMeTemplate },
 };
 
 pub async fn home() -> impl IntoResponse {
-    let template = HomeTemplate {
-        site_name: "logfort".to_string(),
-    };
+    let template = HomeTemplate;
 
     Html(template.render().unwrap())
 }
 
 pub async fn contact_info() -> impl IntoResponse {
-    let template = ContactInfoTemplate {
-        site_name: "logfort".to_string(),
-    };
+    let template = ContactInfoTemplate;
+
+    Html(template.render().unwrap())
+}
+
+pub async fn about_me() -> impl IntoResponse {
+    let template = AboutMeTemplate;
 
     Html(template.render().unwrap())
 }
