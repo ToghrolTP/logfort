1595073074a1690be4b7e9805499b6530a3dc48b
toghroltp <toghroltheprogrammer@gmail.com>
2025-12-02 13:04:43 +0330

    Set global variable through "WithSiteContext" trait, passing these global variables to templates

--- src/templates/home.rs
+++ src/templates/home.rs
@@ -1,7 +1,24 @@
 use askama::Template;
 
+pub trait WithSiteContext {
+    fn site_name(&self) -> &'static str {
+        "LogforT"
+    }
+}
+
 #[derive(Template)]
 #[template(path="home.html")]
-pub struct HomeTemplate {
-    pub site_name: String,
-}
+pub struct HomeTemplate;
+
+impl WithSiteContext for HomeTemplate {}
+
+#[derive(Template)]
+#[template(path="contact-info.html")]
+pub struct ContactInfoTemplate;
+
+impl WithSiteContext for ContactInfoTemplate {}
+
+#[derive(Template)]
+#[template(path="about_me.html")]
+pub struct AboutMeTemplate;
+impl WithSiteContext for AboutMeTemplate {}
