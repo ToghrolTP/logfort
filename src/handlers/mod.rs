813c5e2135c01792cca013dab5bda2e616d758d8
toghroltp <toghroltheprogrammer@gmail.com>
2025-12-02 12:54:49 +0330

    Configure "about_me" route

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -8,5 +8,6 @@ pub fn setup_routes() -> Router {
     Router::new()
         .route("/", get(handlers::home::home))
         .route("/contact-info", get(handlers::home::contact_info))
+        .route("/about-me", get(handlers::home::about_me))
         .nest_service("/static", ServeDir::new("static"))
 }
