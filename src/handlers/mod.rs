8d0e2acbd43ce7470856b4e8d52a912e9b423c73
toghroltp <toghroltheprogrammer@gmail.com>
2025-12-27 17:40:34 +0330

    Add a compression layer for better efficiency

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,7 +1,8 @@
 use crate::handlers;
 use axum::{Router, routing::get};
-use sqlx::{Pool, MySql};
+use sqlx::{MySql, Pool};
 use tower_http::services::ServeDir;
+use tower_http::compression::CompressionLayer;
 
 pub mod home;
 
@@ -10,6 +11,8 @@ pub fn setup_routes(pool: Pool<MySql>) -> Router {
         .route("/", get(handlers::home::home))
         .route("/contact-info", get(handlers::home::contact_info))
         .route("/about-me", get(handlers::home::about_me))
-        .route("/blog", get(handlers::home::blog)).with_state(pool)
+        .route("/blog", get(handlers::home::blog))
+        .with_state(pool)
         .nest_service("/static", ServeDir::new("static"))
+        .layer(CompressionLayer::new())
 }
