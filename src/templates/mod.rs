0fd2680a023dec5741c528c5d06312debff87a62
toghroltp <toghroltheprogrammer@gmail.com>
2026-01-01 06:41:48 +0330

    Add a default method to WithSiteContext that calculates the current year dynamically for a footer copyright.

--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -1,8 +1,14 @@
+use chrono::{Datelike, Local};
+
 pub mod blog;
 pub mod home;
 
 pub trait WithSiteContext {
     fn site_name(&self) -> &'static str {
         "LogforT"
+    }
+
+    fn current_year(&self) -> i32 {
+        Local::now().year()
     }
 }
